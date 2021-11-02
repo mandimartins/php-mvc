862f47d8199943846fe061d17805baed65e66f29
mandimartins <mandimartins23@gmail.com>
2021-11-02 17:02:59 -0300

    feat: add includes file

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,6 +1,8 @@
 <?php
-
+    require APPROOT . "/views/includes/header.php";
 ?>
 <h1><?php echo $data["title"]; ?></h1>
 
-<?php echo APPROOT ?>
+<?php
+    require APPROOT . "/views/includes/footer.php";
+?>
