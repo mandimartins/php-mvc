ab3f88df0750fbf7e65548c4800b4788a303d7dc
mandimartins <mandimartins23@gmail.com>
2021-11-02 14:46:28 -0300

    initiate the core library

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,3 +1,5 @@
 <?php
-    #Require every necessary file
-    echo "I AM BOOTSTRAP FILE";
+    #Require every necessary file / Load libraries
+   require_once "libraries/Core.php";
+   require_once "libraries/Controller.php";
+   require_once "libraries/Database.php";
