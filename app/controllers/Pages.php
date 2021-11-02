c680e3a0cec67e38b683a4bd4f94bcd04debedcb
mandimartins <mandimartins23@gmail.com>
2021-11-02 16:14:06 -0300

    feat: add base controller class

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,6 +1,6 @@
 <?php
 
-class Pages
+class Pages extends Controller
 {
     public function __construct()
     {
@@ -8,6 +8,7 @@ class Pages
 
     public function index()
     {
+        $this->view("hello");
     }
 
     public function about()
