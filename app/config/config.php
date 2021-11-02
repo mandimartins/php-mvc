41e010bea4faa3fe3d19beafdce65ce67bff0daf
mandimartins <mandimartins23@gmail.com>
2021-11-02 16:48:58 -0300

    add a config file and autoloader

---
+++ app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+    //App Root
+    define("APPROOT", dirname(dirname(__FILE__)));
+
+    //URL Root
+    define("URLROOT", "http://localhost/mvcphp");
+
+    //Site Name
+    define("SITENAME", "php mvc");
