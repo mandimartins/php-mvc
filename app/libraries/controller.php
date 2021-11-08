f377117751d919fe67a2c97eae7f87649dd2d41c
mandimartins <mandimartins23@gmail.com>
2021-11-08 19:29:50 -0300

    fix: remove unchecked variable error

--- app/libraries/controller.php
+++ app/libraries/controller.php
@@ -1,33 +1,30 @@
 <?php
 
-# Base Controller
-# Load models and views from this core controller
-
-class controller
+  /*
+   * Base Controller
+   * Loads the models and views
+   */
+class Controller
 {
-    //Load model
+    // Load model
     public function model($model)
     {
-        //Require model file
-        require_once "../app/models/" . $model . ".php";
+      // Require model file
+        require_once '../app/models/' . $model . '.php';
 
-        //Instantiate the model
-
+      // Instatiate model
         return new $model();
     }
 
-    //Load view
-
+    // Load view
     public function view($view, $data = [])
     {
-        //Check for view file
-
-        if (file_exists("../app/views/" . $view . ".php")) {
-            require_once "../app/views/" . $view . ".php";
+      // Check for view file
+        if (file_exists('../app/views/' . $view . '.php')) {
+            require_once '../app/views/' . $view . '.php';
         } else {
-            //View does not exist
-
-            die("View does not exists");
+          // View does not exist
+            die('View does not exist');
         }
     }
 }
--- app/libraries/core.php
+++ app/libraries/core.php
@@ -1,66 +1,60 @@
 <?php
 
-    #Look at the url and pull out unwanted characters
-
-    /**
-     * App core Class
-     * Creates URL & loads core controller
-     * URL FORMAT - /controller/method/params
-     */
-
+  /*
+   * App Core Class
+   * Creates URL & loads core controller
+   * URL FORMAT - /controller/method/params
+   */
 class Core
 {
-    protected $currentController =  "Pages";
-    protected $currentMethod = "index";
+    protected $currentController = 'Pages';
+    protected $currentMethod = 'index';
     protected $params = [];
 
     public function __construct()
     {
-       // print_r( $this->getUrl());
+      //print_r($this->getUrl());
+
         $url = $this->getUrl();
 
-       //Look in controllers for first value
-        if (isset($url[0]) && file_exists('../app/controllers/' . ucwords($url[0]) . "php")) {
-                //If exists, set as controller
-                $this->currentController = ucwords($url[0]);
-
-                //Unset 0 Index
-                unset($url[0]);
+      // Look in controllers for first value
+        if (!empty($url[0]) && file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+          // If exists, set as controller
+            $this->currentController = ucwords($url[0]);
+          // Unset 0 Index
+            unset($url[0]);
         }
 
-            //Require the controller
-             require_once "../app/controllers/" . $this->currentController . ".php";
+      // Require the controller
+        require_once '../app/controllers/' . $this->currentController . '.php';
 
-            //Instantiate controller class
-             $this->currentController = new $this->currentController();
+      // Instantiate controller class
+        $this->currentController = new $this->currentController();
 
-            //Check for second part of url
+      // Check for second part of url
         if (isset($url[1])) {
-            //Check if method exists in controller
+          // Check to see if method exists in controller
             if (method_exists($this->currentController, $url[1])) {
                 $this->currentMethod = $url[1];
+              // Unset 1 index
                 unset($url[1]);
             }
         }
 
-            //Get params
-             $this->params = $url ? array_values($url) : [];
+      // Get params
+        $this->params = $url ? array_values($url) : [];
 
-            // Call a calllback with array of params
-             call_user_func_array(
-                 [$this->currentController, $this->currentMethod],
-                 $this->params
-             );
+      // Call a callback with array of params
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 
     public function getUrl()
     {
         if (isset($_GET['url'])) {
-                $url = rtrim($_GET["url"], "/");
-                $url = filter_var($url, FILTER_SANITIZE_URL);
-                $url = explode('/', $url);
-
-                return $url;
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
         }
     }
 }
