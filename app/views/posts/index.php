b321c0ea84b7e494caa1ab3c1e0b4df13850ccc7
mandimartins <mandimartins23@gmail.com>
2021-11-15 12:27:49 -0300

    feat: list all posts

--- app/views/posts/index.php
+++ app/views/posts/index.php
@@ -20,6 +20,36 @@
         </svg> Add Post
          </a>
     </div>
+    <div class="row mb-3">
+        <?php foreach ($data["posts"] as $post) : ?>
+            <div class="col-md-6">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+        <div class="col p-4 d-flex flex-column position-static">
+          <strong class="d-inline-block mb-2 text-success">Design</strong>
+          <h3 class="mb-0"><?php echo $post->title; ?></h3>
+          <div class="mb-1 text-muted"><?php echo $post->postCreated; ?></div>
+          <div class="mb-1 text-muted">Written by: <strong class="text-dark"><?php echo $post->name; ?></strong></div>
+          <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
+          <a href="<?php echo APPROOT; ?>/posts/show/" class="stretched-link">Continue reading</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <svg class="bd-placeholder-img"
+               width="200"
+               height="250"
+               xmlns="http://www.w3.org/2000/svg"
+               role="img" aria-label="Placeholder: Thumbnail"
+               preserveAspectRatio="xMidYMid slice"
+               focusable="false">
+               <title>Placeholder</title>
+               <rect width="100%"
+                     height="100%"
+                     fill="#55595c">
+               </rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+        </div>
+      </div>
+    </div>
+        <?php endforeach; ?>
+    </div>
 </div>
 
 
