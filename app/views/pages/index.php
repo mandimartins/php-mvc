bcf33c884d854bb6c00a0d772d3934f474d19714
mandimartins <mandimartins23@gmail.com>
2021-11-15 14:39:12 -0300

    fix: show post author

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -7,7 +7,7 @@
     <div class="col-md-6 px-0">
       <h1 class="display-4 fst-italic"><?php echo $data["posts"][0]->title; ?></h1>
       <p class="lead my-3"><?php echo substr($data["posts"][0]->body, 0, 150); ?></p>
-      <p class="lead mb-0"><a href="#" class="text-white fw-bold">Continue reading...</a></p>
+      <p class="lead mb-0"><a href="users/login" class="text-white fw-bold">Continue reading...</a></p>
     </div>
   </div>
 
@@ -15,11 +15,11 @@
     <div class="col-md-6">
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-primary">World</strong>
+          <strong class="d-inline-block mb-2 text-primary"><?php echo $data["posts"][1]->name ?></strong>
           <h3 class="mb-0"><?php echo $data["posts"][1]->title; ?></h3>
           <div class="mb-1 text-muted"><?php echo ($data["posts"][1]->postCreated); ?></div>
-          <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
+          <p class="card-text mb-auto"><?php echo substr($data["posts"][1]->body, 0, 100); ?></p>
+          <a href="users/login" class="stretched-link">Continue reading</a>
         </div>
         <div class="col-auto d-none d-lg-block">
           <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
@@ -30,11 +30,11 @@
     <div class="col-md-6">
       <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
         <div class="col p-4 d-flex flex-column position-static">
-          <strong class="d-inline-block mb-2 text-success">Design</strong>
+          <strong class="d-inline-block mb-2 text-success"><?php echo $data["posts"][2]->name ?></strong>
           <h3 class="mb-0"><?php echo $data["posts"][2]->title; ?></h3>
           <div class="mb-1 text-muted"><?php echo ($data["posts"][2]->postCreated); ?></div>
-          <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="stretched-link">Continue reading</a>
+          <p class="mb-auto"><?php echo substr($data["posts"][2]->body, 0, 100); ?></p>
+          <a href="users/login" class="stretched-link">Continue reading</a>
         </div>
         <div class="col-auto d-none d-lg-block">
           <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"></rect><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
