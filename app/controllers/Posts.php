114d2c2aacc62161f0f964165b25fb8623a2af2e
mandimartins <mandimartins23@gmail.com>
2021-11-16 16:13:38 -0300

    feat: update post

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -4,7 +4,7 @@ class Posts extends Controller
 {
 
     //I wanna protect the whole posts functionalities.
-   // That's why I redirect all accesses except logged in users
+   // That"s why I redirect all accesses except logged in users
     public function __construct()
     {
         if (!isLoggedIn()) {
@@ -12,7 +12,7 @@ class Posts extends Controller
         }
 
         $this->postModel = $this->model("Post");
-        $this->userModel = $this->model('User');
+        $this->userModel = $this->model("User");
     }
 
     public function index()
@@ -64,6 +64,59 @@ class Posts extends Controller
             ];
 
             $this->view("posts/add", $data);
+        }
+    }
+
+    public function edit($id)
+    {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          // Sanitize POST array
+            $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data = [
+            "id" => $id,
+            "title" => trim($_POST["title"]),
+            "body" => trim($_POST["body"]),
+            "user_id" => $_SESSION["user_id"],
+            "title_err" => "",
+            "body_err" => ""
+            ];
+
+          // Validate data
+            if (empty($data["title"])) {
+                $data["title_err"] = "Please enter title";
+            }
+            if (empty($data["body"])) {
+                $data["body_err"] = "Please enter body text";
+            }
+
+          // Make sure no errors
+            if (empty($data["title_err"]) && empty($data["body_err"])) {
+              // Validated
+                if ($this->postModel->updatePost($data)) {
+                    flash("post_message", "Post Updated");
+                    redirect("posts");
+                } else {
+                    die("Something went wrong");
+                }
+            } else {
+              // Load view with errors
+                $this->view("posts/edit", $data);
+            }
+        } else {
+            $post = $this->postModel->getPostById($id);
+
+            if ($post->user_id != $_SESSION["user_id"]) {
+                redirect("posts");
+            }
+
+            $data = [
+            "id" => $id,
+            "title" => $post->title,
+            "body" => $post->body
+            ];
+
+            $this->view("posts/edit", $data);
         }
     }
 
